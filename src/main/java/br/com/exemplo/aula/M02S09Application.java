019b7efb48725bd26dfe43ffc2d4013cad9e30cb
CrisTinaFLN <crisjf.tina@hotmail.com>
2024-08-08 04:36:26 -0300

    Ex1: SecurityConfig

---
+++ src/main/java/br/com/exemplo/aula/M02S09Application.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.aula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M02S09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M02S09Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/exemplo/aula/M02S09ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exemplo.aula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M02S09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
